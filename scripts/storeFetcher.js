87315c0dbcf48bf8b7fd00980ee40700db90bb29
pawel.niechoda <pawel.niechoda@avaus.fi>
2015-02-17 19:21:20 +0200

    Move scripts to scripts directory

---
+++ scripts/storeFetcher.js
@@ -0,0 +1,22 @@
+var Promise = require('promise'),
+    StoreStorage = require('../src/StoreStorage'),
+    AlkoStoreBackend = require('../src/AlkoStoreBackend'),
+    GoogleGeocoderApi = require('../src/GoogleGeocoderApi'),
+    dbConnectionManager = require('../src/DBConnectionManager');
+
+var alkoStoreBackend = new AlkoStoreBackend(),
+    geocoderApi = new GoogleGeocoderApi(),
+    storeStorage = new StoreStorage();
+
+dbConnectionManager.getDB().then(function(db) {
+    return storeStorage.init(db)
+}).then(function() {
+    return alkoStoreBackend.fetchStores()
+}).then(function(stores){
+    var insertOrUpdate = storeStorage.insertOrUpdate.bind(storeStorage);
+    return Promise.all(stores.map(insertOrUpdate));
+}).then(function() {
+    dbConnectionManager.close();
+});
+
+
