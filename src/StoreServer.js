0f25a1bc2cfc243dd14b07f561ec738d463a054c
pawel.niechoda <pawel.niechoda@avaus.fi>
2015-02-21 15:47:23 +0200

    Add StoreServer module

---
+++ src/StoreServer.js
@@ -0,0 +1,45 @@
+var express = require('express'),
+    Promise = require('promise'),
+    StoreStorage = require('./StoreStorage'),
+    dbConnectionManager = require('./DBConnectionManager');
+
+function StoreServer(){};
+
+StoreServer.prototype._prepareStorage = function() {
+    var self = this;
+    self.storeStorage = new StoreStorage;
+    return dbConnectionManager.getDB().then(function(db) {
+        return self.storeStorage.init(db);
+    });
+};
+
+StoreServer.prototype._setRouting = function() {
+    var self = this;
+    self.app.get('/', function(req, res) {
+        self.storeStorage.get().then(function(stores) {
+            res.json(stores);
+        });
+    });
+};
+
+StoreServer.prototype._startServer = function() {
+    var self = this;
+    return new Promise(function(resolve, reject) {
+        self.server = self.app.listen(self.conf.port, function() {
+            resolve(self.server);
+        });
+    });
+};
+
+StoreServer.prototype.init = function(conf) {
+    var self = this;
+    self.app = express();
+    self.conf = conf;
+    return self._prepareStorage().then(function() {
+        self._setRouting();
+        return self._startServer();
+    });
+};
+
+
+module.exports = StoreServer
